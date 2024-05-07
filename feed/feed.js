96cfdea6eb0b44dd89a0252ca3424f5933da584c
Ester Gomes <129803263+estergcarvalho@users.noreply.github.com>
2024-05-07 14:20:58 -0300

    Loading (#40)
    * feat: Adicionar loading no atividades recentes, playlist spotify, tabnews
    
    * refactor: Removendo linha vazia
    
    * refactor: Refaturando funcoes de loading

--- feed/feed.js
+++ feed/feed.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
     reacaoCutir();
     reacaoComentario();
     comentar();
+    carregarAtividadesRecentes();
+    carregarMusicas();
+    carregarTabNews();
 });
 
 function reacaoCutir() {
-    $(".card-reactions-like").click(function(e) {
+    $(".card-reactions-like").click(function (e) {
         e.preventDefault();
 
         var id = $(this).prop("id");
@@ -15,7 +18,7 @@ function reacaoCutir() {
 }
 
 function reacaoComentario() {
-    $(".card-reactions-comment").click(function(e) {
+    $(".card-reactions-comment").click(function (e) {
         e.preventDefault();
 
         var id = $(this).prop("id");
@@ -24,8 +27,8 @@ function reacaoComentario() {
     });
 }
 
-function comentar() {  
-    $(".card-comment-text-area").on("keyup", function() {
+function comentar() {
+    $(".card-comment-text-area").on("keyup", function () {
         $(this).siblings("#btnComentar").hide();
 
         var texto = $(this).val().trim();
@@ -34,5 +37,34 @@ function comentar() {
             $(this).siblings("#btnComentar").show();
         }
     });
-   
+}
+
+function carregarAtividadesRecentes() {
+    var cardTimeline = $(".card-timeline-activity-body");
+
+    cardTimeline.LoadingOverlay("show");
+
+    setTimeout(function () {
+        cardTimeline.LoadingOverlay("hide");
+    }, 3000);
+}
+
+function carregarMusicas() {
+    var cardPlaylistMusica = $(".card-playlist-music-body");
+
+    cardPlaylistMusica.LoadingOverlay("show");
+
+    setTimeout(function () {
+        cardPlaylistMusica.LoadingOverlay("hide");
+    }, 3000);
+}
+
+function carregarTabNews() {
+    var cardTabNews = $(".card-tab-news-body");
+
+    cardTabNews.LoadingOverlay("show");
+
+    setTimeout(function () {
+        cardTabNews.LoadingOverlay("hide");
+    }, 3000);
 }
