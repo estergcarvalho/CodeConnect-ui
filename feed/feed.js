bbcd691ae7d5cfcfdc449bd6102d86cc532a5b4d
estergcarvalho <ester.estudosti@gmail.com>
2024-05-07 18:33:57 -0300

    feat: Adicionar expansao do textarea feed

--- feed/feed.js
+++ feed/feed.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     reacaoCutir();
     reacaoComentario();
     comentar();
+    fazerComentario();
+    fazerPublicacao();
 });
 
 function reacaoCutir() {
     $(".card-reactions-like").click(function(e) {
         e.preventDefault();
-
-        var id = $(this).prop("id");
 
         $(this).find("i.bi-heart").removeClass("bi-heart").addClass("bi-heart-fill");
     });
@@ -20,11 +20,11 @@ function reacaoComentario() {
 
         var id = $(this).prop("id");
 
-        $("#card-comment-text-" + id).focus();
+        $("#card-comment-text-1" + id).focus();
     });
 }
 
-function comentar() {  
+function comentar() {
     $(".card-comment-text-area").on("keyup", function() {
         $(this).siblings("#btnComentar").hide();
 
@@ -34,5 +34,30 @@ function comentar() {
             $(this).siblings("#btnComentar").show();
         }
     });
-   
+
 }
+
+function fazerComentario() {
+    $("#card-comment-text-1").on('input', function() {
+        $(this).css('height', this.scrollHeight + 'px');
+
+        var texto = $(this).val().trim();
+
+        if (texto === "") {
+            $(this).css('height', '');
+        }
+    });
+}
+
+function fazerPublicacao() {
+    $("#publicacao").on('input', function() {
+        $(this).css('height', this.scrollHeight + 'px');
+
+        var texto = $(this).val().trim();
+
+        if (texto === "") {
+            $(this).css('height', '');
+        }
+    });
+}
+
