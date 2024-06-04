2d109ebe46d563d228613b12c1981f1f7e28a3cf
Ester Gomes <129803263+estergcarvalho@users.noreply.github.com>
2024-06-04 10:21:41 -0300

    feat: Integracao login com backend (#47)
    * feat: Integracao login com backend
    
    * refactor: Alteracao de nome da funcao para validarToken
    
    * refactor: Correcoes de funcoes login

--- login/login.js
+++ login/login.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-    $('#form-login').submit(function (event) {
+    $('#form-login').submit(function(event) {
         event.preventDefault();
 
-        validaCampos();
-
+        if(validaCampos()) {
+            login();
+        }
     });
 });
 
@@ -11,11 +12,35 @@ function validaCampos() {
     var email = $("#email").val();
     var senha = $("#senha").val();
 
-    if(email.trim() === ""){
-        alert("É necessário preencher o campo e-mail.");
+    if (email.trim() === "") {
+        alert("É necessário preencher o campo e-mail");
+        return false;
     }
 
-    if(senha.trim() === ""){
+    if (senha.trim() === "") {
         alert("É necessário preencher o campo de senha");
+        return false;
     }
+
+    return true;
+}
+
+function login() {
+    var email = $("#email").val();
+    var senha = $("#senha").val();
+
+    $.ajax({
+        url: "http://localhost:8080/login",
+        type: 'POST',
+        contentType: "application/json",
+        dataType: 'json',
+        data: JSON.stringify({ email, senha }),
+        success: function(response) {
+            localStorage.setItem('token', response.access_token);
+            window.location.href = '/feed/feed.html';
+        },
+        error: function(xhr, status, error) {
+            alert('Erro ao fazer login', error);
+        }
+    });
 }
