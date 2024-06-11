01377f8fcf4c71b0401d6dae1d08ad26fd9d9368
Ester Carvalho <129803263+estergcarvalho@users.noreply.github.com>
2024-06-11 20:49:47 -0300

    feat: Listagem de amigos e integracao com backend (#51)
    * feat: Listagem de amigos e integracao com backend
    
    * refactor: Exclusao de funcao setTimeout amigos e correcao na funcao amigos
    
    * refactor: Correcao codigo funcao amigos adicionando p no link

---
+++ amigos/amigos.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    listarAmigos();
+});
+
+function listarAmigos() {
+    var amigosUsuario = $(".card-post-list-friends-loading");
+    amigosUsuario.fadeIn();
+    amigosUsuario.LoadingOverlay("show");
+
+    var token = localStorage.getItem('token');
+
+    $.ajax({
+        url: "http://localhost:8080/usuarios/amigos",
+        type: 'GET',
+        dataType: "json",
+        headers: {'Authorization': 'Bearer ' + token},
+        success: function(data) {
+            var amigos = data.amigos;
+            var totalAmigos = data.total;
+
+            $('#total-amigos').html('(' + totalAmigos + ')');
+            
+            amigos.forEach(function(amigo) {
+                $('#lista-amigos').append(
+                '<div class="card-friends-content align-items-center text-center col-2"> ' +
+                    '<div class="card-friends-user">' +
+                        '<a class="card-friends-avatar" href="/perfil/perfil.html">' +
+                            '<img class="img-thumbnail rounded-circle" alt="image-user" src="/assets/img/usuarios/luiz.jpg">' +
+                            '<p class="card-friends-user-name mb-1">' + amigo.nome + '</p> ' +
+                        '</a>' +
+                        '<span></span>' +
+                    '</div>' +
+                '</div>' 
+                );
+            });
+
+            var listarAmigos = $(".card-post-list-friends-loading");
+            listarAmigos.LoadingOverlay("hide");
+        },
+        error: function(status, response, error) {
+            console.error("Erro ao listar amigos" + error);
+        }
+    });
+}
