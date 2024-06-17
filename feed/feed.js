83573fc2c73ceb2d6b86bdf0172ca862f603330f
Ester Carvalho <129803263+estergcarvalho@users.noreply.github.com>
2024-06-17 18:15:39 -0300

    feat: Retornar lista de postagens usuario logado (#56)
    * feat: Retornar lista de postagens usuario logado
    
    * refactor: Correcao do  html no js
    
    * refactor: Exclusao de loading funcao ListarPostagens

--- feed/feed.js
+++ feed/feed.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     fazerPublicacao();
     publicarFeed();
     habilitarPublicacao();
+    listarPostagens();
 });
 
 function publicarFeed() {
@@ -183,9 +184,99 @@ function habilitarPublicacao() {
         var publicacao = $(this).val().trim();
 
         if (publicacao === "") {
-            habilitarBtn.attr('disabled'); 
+            habilitarBtn.attr('disabled');
         } else {
-            habilitarBtn.removeAttr('disabled'); 
+            habilitarBtn.removeAttr('disabled');
         }
     });
+}
+
+function listarPostagens() {
+    var token = localStorage.getItem('token');
+
+    $.ajax({
+        url: "http://localhost:8080/posts",
+        type: 'GET',
+        dataType: "json",
+        headers: {'Authorization': 'Bearer ' + token},
+        success: function(data) {
+            data.forEach(function(post) {
+                if (data != null) {
+                $('#lista-postagem').prepend(
+                        `<div class="card-post card">
+                            <div class="card-header">
+                                <div class="row">
+                                    <div class="card-post-avatar col-2">
+                                        <a href="/perfil/perfil.html">
+                                            <img src="/assets/img/usuarios/caique.jpg" alt="Foto de Caique">
+                                        </a>
+                                    </div>
+                                    
+                                    <div class="col-10 position-relative">
+                                        <div class="card-post-user-name position-absolute top-50 translate-middle">
+                                            <div>
+                                                <a href="/perfil/perfil.html">Caique Nogueira</a>
+                                            </div>  
+                                            <div class="card-post-profession">
+                                                    Engenheiro Software
+                                            </div>  
+                                            <div class="card-post-date">
+                                                    1 hora
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card-body">
+                                <p class="card-post-text">
+                                    `+ post.descricao +`
+                                </p>
+                            </div>
+                            <div class="card-footer pb-0">
+                                <div class="card-reactions-count">
+                                    <a href="#" class="card-reactions-count-likes">0 curtidas</a>
+                                    <a href="#">0 comentários</a>
+                                </div>
+
+                                <hr class="card-line card-line-count">
+
+                                <div class="card-reactions">
+                                    <a class="card-reactions-like" href="#" id="0">
+                                        <i class="bi bi-heart" id="img-like-1"></i>
+                                        <span>Curtir</span>
+                                    </a>
+
+                                    <a class="card-reactions-comment" href="#" id="0">
+                                        <i class="bi bi-chat"></i>
+                                        <span>Comentar</span>
+                                    </a>
+
+                                    <a href="#">
+                                        <i class="bi bi-share"></i>
+                                        <span>Compartilhar</span>
+                                    </a>
+                                </div>
+
+                                <hr class="card-line card-line-reactions">
+
+                                <div class="card-comment">
+                                    <img class="card-comment-avatar" src="/assets/img/usuarios/ana.jpg" alt="Foto de ana">
+
+                                    <div class="card-comment-text mb-3">
+                                        <textarea class="card-comment-text-area" id="card-comment-text-2" placeholder="Escreva um comentário..."></textarea>
+                                        <label class="form-label d-none" for="card-comment-text-1"></label>
+                                        <button class="card-comment-btn" id="btnComentar" type="button">Comentar</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+                    );
+                }
+            });
+        },
+        error: function(status, response, error) {
+            console.error("Erro ao listar postagens" + error);
+        }
+    });                  
 }
